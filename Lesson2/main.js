46b42b701d22b3354c7b95134fc87d037f7801fa
MaverickLviv <pkrainikov@gmail.com>
2021-07-19 18:36:37 +0300

    3 tasks 15.07.2021 9 task concСделал (с ошибкой) испраляю ошибки
    new file_dop
    2 task
    colors inut
    
    ppp
    2 lesson
    
    1 task
    перенос в main.js

--- Lesson2/main.js
+++ Lesson2/main.js
@@ -1,3 +1,21 @@
+<!--        template 1.1-->
+// <ul>
+//     <li>ITEM OF ARRAY</li>
+//     <!--
+//     інші об'єкти масиву
+//   ...
+//   ...
+//   ...
+//     -->
+// </ul>
+//
+// /*
+//             Використовуючи данні з масиву, за допомоги document.write та циклу
+//             побудувати структуру по шаблону template1.1
+//             */
+
+
+
 let listOfItems =['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
 document.write('<div>')
 for (const listOfItems of ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js']) {
@@ -6,3 +24,24 @@ for (const listOfItems of ['html', 'css', 'javascript', 'mysql', 'mongodb', 'rea
 
     document.write('/<div>')
 }
+// Використовуючи данні з масиву, за допомоги document.write та циклу
+// побудувати структуру по шаблону template2.1 & template2.2. Зробити адекватну стилізацію.
+//     Великими літерами прописанні властивості об'єкту які потрібно впровадити в шаблон.
+// Шаблон застосувати до кожного об'єкта в масиві
+// */
+
+
+    //{name: vasya, surname: petrenko, age: 20, info: html}
+
+
+    let listOfUsers
+    document.write(<div>)
+
+        let listOfUsers =[name: 'vasya', surname: 'ivanov', age: 20, info: 'html'];
+        let listOfUsers =[name: 'vova', surname: 'petrov', age: 25, info: 'java'];
+        let listOfUsers =[name: 'olya', surname: 'sidorov', age: 30, info:'c++'];
+
+
+    document.write(</div>)
+
+</div>
