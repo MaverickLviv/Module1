5066d09b9069e2fd9e8fd6c9fb4adcf32803141f
MaverickLviv <pkrainikov@gmail.com>
2021-07-19 18:51:29 +0300

    3 tasks 15.07.2021 9 task concСделал (с ошибкой) испраляю ошибки
    new file_dop
    2 task
    colors inut
    
    ppp
    2 lesson
    
    1 task
    
    remove main.js
    + massive

--- Lesson2/main.js
+++ Lesson2/main.js
@@ -34,25 +34,25 @@ for (const listOfItems of ['html', 'css', 'javascript', 'mysql', 'mongodb', 'rea
     //{name: vasya, surname: petrenko, age: 20, info: html}
 
 
-    let listOfUsers = [
+    let listOfUsers =[
         {
             name: 'vasya',
             surname: 'ivanov',
             age: 20,
             info: 'https://342031.selcdn.ru/rusplt/images/06072020/1594031375780-upload.jpeg'
-        }
+        },
         {
             name: 'vova',
             surname: 'petrov',
             age: 25,
             info: 'https://kinoreporter.ru/wp-content/uploads/2020/10/TASS_3118768.jpg'
-        }
+        },
         {
             name: 'olya',
             surname: 'sidorov',
             age: 30,
             info:'https://www.soyuz.ru/public/uploads/files/2/7146846/20170926123640bd34f04737.jpg'
-        }
+        },
 
 ]
         document.write(<div>)
