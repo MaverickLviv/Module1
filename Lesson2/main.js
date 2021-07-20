d7526ea797b768d49c39d58b5ea6db6de5e54a41
MaverickLviv <pkrainikov@gmail.com>
2021-07-20 18:34:05 +0300

    3 tasks 15.07.2021 9 task concСделал (с ошибкой) испраляю ошибки
    new file_dop
    2 task
    colors inut
    
    ppp
    2 lesson
    
    1 task
    
    remove main.js
    + massive
    
    task2_end

--- Lesson2/main.js
+++ Lesson2/main.js
@@ -34,7 +34,7 @@ for (const listOfItems of ['html', 'css', 'javascript', 'mysql', 'mongodb', 'rea
     //{name: vasya, surname: petrenko, age: 20, info: html}
 
 
-    let listOfUsers =[
+    let heroes =[
         {
             name: 'vasya',
             surname: 'ivanov',
@@ -55,8 +55,124 @@ for (const listOfItems of ['html', 'css', 'javascript', 'mysql', 'mongodb', 'rea
         },
 
 ]
-        document.write(<div>)
 
-         document.write(</div>)
+        document.write('<div>');
+        for (const user of heroes) {
+         document.write(
+        `<div class="user">
+                    ${user.name} ${user.surname} ${user.age}  ${user.info}  <img src="${user.photo}" alt="">
+            </div>`);
+
+
+        }
+//2-2
+//     <!--template 2.2-->
+//     <div>
+//         <h2>NAME SURNAME. age is - AGE</h2>
+//         <p>INFO</p>
+//         <img src="PHOTO" alt="">
+//     </div>
+//     <!--
+//     інші об'єкти з масиву
+//     ...
+//     ...
+//     ...
+//     -->
+for (const user of heroes) {
+    document.write(
+        `<div class="user2">
+                           <h2> ${user.name}  ${user.surname} age is ${user.age}</h2>
+                           <p> ${user.info}</p>
+                           <img src="${user.photo}" alt="">
+            </div>`);
+}
+// <!--template 2.3-->
+// <div>
+//     <div>
+//         <h2>NAME SURNAME. age is - AGE</h2>
+//         <p>INFO</p>
+//         <img src="PHOTO" alt="">
+//     </div>
+//     <!--
+// інші об'єкти з масиву
+//     ...
+//     ...
+//     ...
+//     ...
+//     -->
+//
+//
+// </div>
+document.write(`<div>`)
+for (const user of heroes) {
+    document.write(
+        `<div class="user3">
+                           <h2> ${user.name}  ${user.surname} age is ${user.age}</h2>
+                           <p> ${user.info}</p>
+                           <img src="${user.photo}" alt="">
+            </div>`);
+}
+document.write(`</div>`)
+
+/*
+            Використовуючи данні з масиву, за допомоги document.write та циклу
+            побудувати структуру по шаблону template3.1 Зробити адекватну стилізацію
+            Великими літерами прописанні властивості об'єкту які потрібно впровадити в шаблон
+                */
+// <!--        template 3.1-->
+// <div>
+//     <div class="product-card">
+//         <h3 class="product-title">TITLE. Price - PRICE</h3>
+//         <img src="" alt="" class="product-image">
+//     </div>
+//     <!--
+//                 інші об'єкти з масиву
+//                 ...
+//                 ...
+//                 ...
+//     -->
+//
+// </div>
+
+let products = [
+    {
+        title: 'milk',
+        price: 22,
+        image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
+    },
+    {
+        title: 'juice',
+        price: 27,
+        image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
+    },
+    {
+        title: 'tomato',
+        price: 47,
+        image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
+    },
+    {
+        title: 'tea',
+        price: 15,
+        image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
+    },
+];
+document.write(`<div>`);
+for (const product of products) {
+    document.write(`<div class="product-card">
+                <h3 class="product-title"> ${product.title} prise - ${product.price} UAN </h3>
+                <img src="${product.image}" alt="" class="product-image">
+                `);
+    document.write(`</div>`);
+
+}
+
+document.write(`</div>`);
+
+
+</script>
+</body>
+</html>
+
+
 
 </div>
