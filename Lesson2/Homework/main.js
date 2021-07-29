5c797ef3cbe786b28a8df5f2af7f4c93e1af52e2
MaverickLviv <pkrainikov@gmail.com>
2021-07-29 16:59:23 +0300

    3 tasks 15.07.2021 9 task concСделал (с ошибкой) испраляю ошибки
    new file_dop
    2 task
    colors inut
    
    ppp
    2 lesson
    
    1 task
    
    remove main.js
    + massive
    
    task2_end
    final
    DZ 3

--- Lesson2/Homework/main.js
+++ Lesson2/Homework/main.js
@@ -16,15 +16,15 @@
 
 
 
-let listOfItems =['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
-document.write('<div>')
-for (const item of listOfItems) {
-
-
-
-    document.write(`<div>${listOfItems}</div>`);
-}
-    document.write('/<div>')
+// let listOfItems =['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+// document.write('<div>')
+// for (const item of listOfItems) {
+//
+//
+//
+//     document.write(`<div>${listOfItems}</div>`);
+// }
+//     document.write('/<div>')
 
 // Використовуючи данні з масиву, за допомоги document.write та циклу
 // побудувати структуру по шаблону template2.1 & template2.2. Зробити адекватну стилізацію.
@@ -36,35 +36,35 @@ for (const item of listOfItems) {
     //{name: vasya, surname: petrenko, age: 20, info: html}
 
 
-    let heroes =[
-        {
-            name: 'vasya',
-            surname: 'ivanov',
-            age: 20,
-            info: 'https://342031.selcdn.ru/rusplt/images/06072020/1594031375780-upload.jpeg'
-        },
-        {
-            name: 'vova',
-            surname: 'petrov',
-            age: 25,
-            info: 'https://kinoreporter.ru/wp-content/uploads/2020/10/TASS_3118768.jpg'
-        },
-        {
-            name: 'olya',
-            surname: 'sidorov',
-            age: 30,
-            info:'https://www.soyuz.ru/public/uploads/files/2/7146846/20170926123640bd34f04737.jpg'
-        },
+    // let heroes =[
+    //     {
+    //         name: 'vasya',
+    //         surname: 'ivanov',
+    //         age: 20,
+    //         info: 'https://342031.selcdn.ru/rusplt/images/06072020/1594031375780-upload.jpeg'
+    //     },
+    //     {
+    //         name: 'vova',
+    //         surname: 'petrov',
+    //         age: 25,
+    //         info: 'https://kinoreporter.ru/wp-content/uploads/2020/10/TASS_3118768.jpg'
+    //     },
+    //     {
+    //         name: 'olya',
+    //         surname: 'sidorov',
+    //         age: 30,
+    //         info:'https://www.soyuz.ru/public/uploads/files/2/7146846/20170926123640bd34f04737.jpg'
+    //     },
+//
+// ]
 
-]
-
-        document.write('<div>');
-        for (const user of heroes) {
-         document.write(
-        `<div class="user">
-                    ${user.name} ${user.surname} ${user.age}  ${user.info}  <img src="${user.photo}" alt="">
-            </div>`);
-
+        // document.write('<div>');
+        // for (const user of heroes) {
+        //  document.write(
+        // `<div class="user">
+        //             ${user.name} ${user.surname} ${user.age}  ${user.info}  <img src="${user.photo}" alt="">
+        //     </div>`);
+        //
 
         }
 //2-2
@@ -80,14 +80,14 @@ for (const item of listOfItems) {
 //     ...
 //     ...
 //     -->
-for (const user of heroes) {
-    document.write(
-        `<div class="user2">
-                           <h2> ${user.name}  ${user.surname} age is ${user.age}</h2>
-                           <p> ${user.info}</p>
-                           <img src="${user.photo}" alt="">
-            </div>`);
-}
+//     for (const user of heroes) {
+//     document.write(
+        `<!--<div class="user2">-->
+<!--                           <h2> ${user.name}  ${user.surname} age is ${user.age}</h2>-->
+<!--                           <p> ${user.info}</p>-->
+<!--                           <img src="${user.photo}" alt="">-->
+            // </div>`);
+// }
 // <!--template 2.3-->
 // <div>
 //     <div>
@@ -105,22 +105,22 @@ for (const user of heroes) {
 //
 //
 // </div>
-document.write(`<div>`)
-for (const user of heroes) {
-    document.write(
-        `<div class="user3">
-                           <h2> ${user.name}  ${user.surname} age is ${user.age}</h2>
-                           <p> ${user.info}</p>
-                           <img src="${user.photo}" alt="">
-            </div>`);
-}
-document.write(`</div>`)
-
-/*
-            Використовуючи данні з масиву, за допомоги document.write та циклу
-            побудувати структуру по шаблону template3.1 Зробити адекватну стилізацію
-            Великими літерами прописанні властивості об'єкту які потрібно впровадити в шаблон
-                */
+// document.write(`<div>`)
+// for (const user of heroes) {
+//     document.write(
+//         `<div class="user3">
+//                            <h2> ${user.name}  ${user.surname} age is ${user.age}</h2>
+//                            <p> ${user.info}</p>
+//                            <img src="${user.photo}" alt="">
+//             </div>`);
+// }
+// document.write(`</div>`)
+//
+// /*
+//             Використовуючи данні з масиву, за допомоги document.write та циклу
+//             побудувати структуру по шаблону template3.1 Зробити адекватну стилізацію
+//             Великими літерами прописанні властивості об'єкту які потрібно впровадити в шаблон
+//                 */
 // <!--        template 3.1-->
 // <div>
 //     <div class="product-card">
@@ -136,45 +136,45 @@ document.write(`</div>`)
 //
 // </div>
 
-let products = [
-    {
-        title: 'milk',
-        price: 22,
-        image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
-    },
-    {
-        title: 'juice',
-        price: 27,
-        image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
-    },
-    {
-        title: 'tomato',
-        price: 47,
-        image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
-    },
-    {
-        title: 'tea',
-        price: 15,
-        image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
-    },
-];
-document.write(`<div>`);
-for (const product of products) {
-    document.write(`<div class="product-card">
-                <h3 class="product-title"> ${product.title} prise - ${product.price} UAN </h3>
-                <img src="${product.image}" alt="" class="product-image">
-                `);
-    document.write(`</div>`);
-
-}
-
-document.write(`</div>`);
-
-
-</script>
-</body>
-</html>
-
-
-
-</div>
+// let products = [
+//     {
+//         title: 'milk',
+//         price: 22,
+//         image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
+//     },
+//     {
+//         title: 'juice',
+//         price: 27,
+//         image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
+//     },
+//     {
+//         title: 'tomato',
+//         price: 47,
+//         image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
+//     },
+//     {
+//         title: 'tea',
+//         price: 15,
+//         image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
+//     },
+// ];
+// document.write(`<div>`);
+// for (const product of products) {
+//     document.write(`<div class="product-card">
+//                 <h3 class="product-title"> ${product.title} prise - ${product.price} UAN </h3>
+//                 <img src="${product.image}" alt="" class="product-image">
+//                 `);
+//     document.write(`</div>`);
+//
+// }
+//
+// document.write(`</div>`);
+//
+//
+// </script>
+// </body>
+// </html>
+//
+//
+//
+// </div>
--- Lesson3/HomeWork3/Main.js
+++ Lesson3/HomeWork3/Main.js
@@ -32,28 +32,62 @@
 // повертає найменьше,// а виводить найбільше (Math використовувати заборонено);
 
 
-function min_max (){
-    let min = arguments[0];
-    let max = arguments[0];
-    for (const item of arguments) {
-        if (item > max) max = item;
-        if (item < min) min = item;
-    }
-    document.write(max);
-    return min;
-}
-console.log(min_max(3,6,7,2,5,23,54,765,35,98,24,6,9,4))
+// function min_max (){
+//     let min = arguments[0];
+//     let max = arguments[0];
+//     for (const item of arguments) {
+//         if (item > max) max = item;
+//         if (item < min) min = item;
+//     }
+//     document.write(max);
+//     return min;
+// }
+// console.log(min_max(3,6,7,2,5,23,54,765,35,98,24,6,9,4))
 
 
 
 // 5 - створити функцію яка  створює блок з текстом. Текст задати через аргумент
-
+//   function createBlock (text){
+//
+//     document.write(`<div><h2>${text}</h2></div>`);
+//     }
+//     createBlock('Done')
 
 // 6 - створити функцію яка  створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
-
+// function createUl(Library){
+//         document.write(`<ul>
+//
+//        <li>text1</li>
+//        <li>text2</li>
+//        <li>text3</li>
+//
+//     </ul>`);
+//
+//     }
+//     createUl('a')
 
 // 7 - створити функцію яка  створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий.
 //     Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
+//     function createUl(a1,r){
+//     document.write(`<ul>`);
+//
+//     for (let i=0; i<r; i++){
+//         document.write('<li>${text}</li>');
+//     }
+//     document.write(`</ul>`);
+// }
+//     createUl(`What can i say, 5`);
+
+//8 - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві),
+// та будує для них список
 
 
-//8 - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
+    // let array =[2, 5, 'Tango', 76, true, 'Charlie', 222, 'Alfa', 'Bravo', false, 'India', ];
+    //
+    // function createA(array){
+    //     for (let i=0; i < array.length; i++), {
+    //     const arrayElement = array['i'];
+    //     document.write(`<li>${arrayElement}</li>`)
+    //     }
+    // }
+    // createA(array);
