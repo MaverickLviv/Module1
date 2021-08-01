7fdf6a1ea04eeff3ca4b9f926fb96aa208b256e9
MaverickLviv <pkrainikov@gmail.com>
2021-08-01 23:22:47 +0300

    3 tasks 15.07.2021 9 task concСделал (с ошибкой) испраляю ошибки
    new file_dop
    2 task
    colors inut
    
    ppp
    2 lesson
    
    1 task
    
    remove main.js
    + massive
    
    task2_end
    final
    DZ 3
    
    ClasWork3 - finished
    
    HomeWork4
    2 t
    task 4

--- Lesson4/HomeWork4/main.js
+++ Lesson4/HomeWork4/main.js
@@ -16,21 +16,37 @@
 // - Змінти попередню функцію, щоб діапазон можна було визначити через
 // аргументи.
 
-let array2 = []
-function getRandomInt(min, max, l) {
-    for (let i = 1; i <= l; i++) {
-        array2.push(Math.floor(Math.random()*(max-min)+min));
-    }
-    return array2
-}
-getRandomInt( 0, 100, 84);
-console.log(array2);
-
-
-
-
+// let array2 = []
+// function getRandomInt(min, max, l) {
+//     for (let i = 1; i <= l; i++) {
+//         array2.push(Math.floor(Math.random()*(max-min)+min));
+//     }
+//     return array2
+// }
+// getRandomInt( 0, 55, 99)
+// console.log(array2);
 
 // - створити (або згенерувати, за допомоги попередньої функції) масив рандомних цілих числових значень. Відсортувати його за допомоги sort
+
+let array4 = []
+function getRandomInt4(min, max, l) {
+    for (let i = 1; i <= l; i++) {
+        array4.push(Math.floor(Math.random()*(max-min)+min));
+    }
+    return array4
+}
+getRandomInt4( 0, 100, 84);
+let sort = array4.sort(function (u1,u2){
+    return u1-u2;
+});
+console.log(sort);
+
+
+
+
+
+
+
 // - створити (або згенерувати, за допомоги попередньої функції) масив рандомних цілих числових значень. відфільтрувати  його за допомоги filter, за лишивши тільки парні числа
 // - створити масив рандомних цілих числових значень (або згенерувати, за допомоги попередньої функції) . за допомоги map та колбеку перетворити всі об'єкти в масиві на срінгові.
 //
