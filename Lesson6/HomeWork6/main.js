026f3b672f724f1c7da7228a69b9ef00ac6d45aa
MaverickLviv <pkrainikov@gmail.com>
2021-08-06 16:06:33 +0300

    3 tasks 15.07.2021 9 task concСделал (с ошибкой) испраляю ошибки
    new file_dop
    2 task
    colors inut
    
    ppp
    2 lesson
    
    1 task
    
    remove main.js
    + massive
    
    task2_end
    final
    DZ 3
    
    ClasWork3 - finished
    
    HomeWork4
    2 t
    task 4
    task 5
    
    Lesson6

--- Lesson6/HomeWork6/main.js
+++ Lesson6/HomeWork6/main.js
@@ -1,8 +1,28 @@
+// <!--1.-->
+// <!--Отримати відповідь з цього ресурсу відповідь, та вивести в документ як в прикладі на занятті-->
+// <!--https://jsonplaceholder.typicode.com/posts-->
+//
+//     <!--2.-->
+//     <!--Отримати відповідь з цього ресурсу відповідь, та вивести в документ як в прикладі на занятті-->
+//     <!--https://jsonplaceholder.typicode.com/comments-->
+
 fetch('https://jsonplaceholder.typicode.com/posts')
     .then(response => response.json())
-    .then(value => console.log(value));
+    .then(posts => {
+        console.log(posts)
+        const postsDiv =document.getElementById('posts');
+        for (const post of posts){
+           const postDiv =document.createElement('div');
+           postDiv.classList.add('post'); /*елемент, свойство, метод*/
+            const postTitle =document.createElement('h2');
+            postTitle.innerText =`${post.id}. ${post.title}`;
+        }
+    } );
+
+
 
 
 fetch('https://jsonplaceholder.typicode.com/comments')
     .then(response => response.json())
     .then(value => console.log(value));
+
