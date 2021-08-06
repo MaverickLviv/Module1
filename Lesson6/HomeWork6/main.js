2904511a45784d686dabe15690c72a3bec0367d4
MaverickLviv <pkrainikov@gmail.com>
2021-08-06 17:14:31 +0300

    3 tasks 15.07.2021 9 task concСделал (с ошибкой) испраляю ошибки
    new file_dop
    2 task
    colors inut
    
    ppp
    2 lesson
    
    1 task
    
    remove main.js
    + massive
    
    task2_end
    final
    DZ 3
    
    ClasWork3 - finished
    
    HomeWork4
    2 t
    task 4
    task 5
    
    Lesson6

--- Lesson6/HomeWork6/main.js
+++ Lesson6/HomeWork6/main.js
@@ -14,15 +14,31 @@ fetch('https://jsonplaceholder.typicode.com/posts')
         for (const post of posts){
            const postDiv =document.createElement('div');
            postDiv.classList.add('post'); /*елемент, свойство, метод*/
-            const postTitle =document.createElement('h2');
+            const postTitle =document.createElement('h2'); /*елементу присваиваю h2*/
             postTitle.innerText =`${post.id}. ${post.title}`;
+            const postBody =document.createElement('p');
+            postBody.innerText =post.body;
+            postDiv.append(postTitle,postBody);
+            postsDiv.append(postDiv);
         }
     } );
 
 
-
-
 fetch('https://jsonplaceholder.typicode.com/comments')
     .then(response => response.json())
-    .then(value => console.log(value));
+    .then(comments => {
+        console.log(comments)
+        const commentsDiv =document.getElementById('comments');
+        for (const comment of comments){
+            const commentDiv =document.createElement('div');
+            commentDiv.classList.add('comment');
+            const commentEmail =document.createElement('h2');
+            commentEmail.innerText =`${comment.id}. ${comment.email}`;
+            const commentBody =document.createElement('p');
+            commentBody.innerText=comment.body;
+            commentDiv.append(commentEmail,commentBody);
+            commentsDiv.append(commentDiv);
+
+        }
+    });
 
