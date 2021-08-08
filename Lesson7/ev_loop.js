3eca6e41954b7ff21d258b094506233eca528106
MaverickLviv <pkrainikov@gmail.com>
2021-08-08 22:43:14 +0300

    3 tasks 15.07.2021 9 task concСделал (с ошибкой) испраляю ошибки
    new file_dop
    2 task
    colors inut
    
    ppp
    2 lesson
    
    1 task
    
    remove main.js
    + massive
    
    task2_end
    final
    DZ 3
    
    ClasWork3 - finished
    
    HomeWork4
    2 t
    task 4
    task 5
    
    Lesson7
    
    Lesson6

--- Lesson7/ev_loop.js
+++ Lesson7/ev_loop.js
@@ -17,7 +17,94 @@ function morningTime(time) {
 
     })
 }
+
+function morningTime1(time) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Встал');
+            resolve();
+        }, 200);
+    } )
+}
+function morningTime2(/*time*/) {
+    return new Promise((resolve, /*reject*/) => {
+        setTimeout(() => {
+            console.log('Курим');
+            resolve();
+        }, 600);
+    } );
+
+}
+function morningTime3(time, water) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Водные процедуры');
+            if (water < 0) {
+                console.log('No bathroom')
+            } else {
+                console.log('Умываться, чистить зубы')
+            }
+            resolve();
+        }, 700);
+    } );
+
+}
+
+function morningTime4(/*time*/) {
+    return new Promise((resolve, /*reject*/) => {
+        setTimeout(() => {
+            console.log('Сборы');
+            resolve();
+        }, 800);
+    } );
+
+}
+function morningTime5(/*time*/) {
+    return new Promise((resolve, /*reject*/) => {
+        setTimeout(() => {
+            console.log('ДОрога на работу');
+            resolve();
+        }, 700);
+    } );
+
+}
+function morningTime6(/*time*/) {
+    return new Promise((resolve, /*reject*/) => {
+        setTimeout(() => {
+            console.log('Работа, отбытие домой');
+            resolve();
+        }, 700);
+    } );
+
+}
+function morningTime7(/*time*/) {
+    return new Promise((resolve, /*reject*/) => {
+        setTimeout(() => {
+            console.log('Ужин, лекция, учеба');
+            resolve();
+        }, 800);
+    } );
+
+}
+function morningTime8(/*time*/) {
+    return new Promise((resolve, /*reject*/) => {
+        setTimeout(() => {
+            console.log('Сон');
+            resolve();
+        }, 900);
+    } );
+
+}
+
 async function allDay (){
     const day = await morningTime(3)
+    await morningTime1();
+    await morningTime2();
+    await morningTime3();
+    await morningTime4();
+    await morningTime5();
+    await morningTime6();
+    await morningTime7();
+    await morningTime8();
 }
 allDay().then();
