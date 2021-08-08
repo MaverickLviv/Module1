76f50e7eab28a3faa65f83505961d31e02493f64
MaverickLviv <pkrainikov@gmail.com>
2021-08-08 15:28:57 +0300

    3 tasks 15.07.2021 9 task concСделал (с ошибкой) испраляю ошибки
    new file_dop
    2 task
    colors inut
    
    ppp
    2 lesson
    
    1 task
    
    remove main.js
    + massive
    
    task2_end
    final
    DZ 3
    
    ClasWork3 - finished
    
    HomeWork4
    2 t
    task 4
    task 5
    
    Lesson7
    
    Lesson6

--- Lesson7/ev_loop.js
+++ Lesson7/ev_loop.js
@@ -6,8 +6,12 @@ function morningTime(time) {
         setTimeout(() => {
                 console.log('Утро, мать его');
                 if (time < 3) {
-                    console.log('')
-
+                    console.log('Спим')
+                    reject('Спать')
+                }
+                else {
+                    console.log('Подъем');
+                    resolve('Встаем');
                 }
             }
         )
